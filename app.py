222c2dc56f921eb724e7cfbb3335125f7ee01830
Kshitij <kshitijchaudhari@gmail.com>
2020-02-05 23:41:09 +0530

    Game Update #2

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from datetime import datetime
 from tkinter import Text, filedialog
 import os
 from random import seed
@@ -6,7 +7,6 @@ from random import randint
 import time
 import threading
 
-from RepeatingTimer import RepeatingTimer
 
 __timer = 5
 __height = 500
@@ -16,22 +16,22 @@ root = tk.Tk()
 
 
 def appStart():
-    t = threading.Thread(target={r.run()})
-    t.start()
+    b1.config(state="disabled")
+    generateApples()
 
 
 def resetScore():
-    r.stop()
+    pass
 
 
 def generateApples():
-    o = seed(int(round(time.time() / 1000)))
-    a = tk.PhotoImage(file="assets/apple.gif")
+    seed()
     x = randint(0, (__width - 20))
     y = randint(0, (__height - 20))
-    s = " x: " + str(x) + " , " + " y: " + str(y) + "   seed: " + str(o)
+    s = " x: " + str(x) + " , " + " y: " + str(y)
     l1.config(text=s)
     canvas.create_image(x, y, image=a, anchor=tk.NW)
+    root.after(3000,generateApples)
 
 
 canvas = tk.Canvas(root, height=__height, width=__width, bg="#213209")
@@ -45,6 +45,8 @@ l1.pack(side="right")
 l2 = tk.Label(root, text="Score: ", justify=tk.LEFT, padx=10)
 l2.pack(side="right")
 
-r = RepeatingTimer(3, generateApples())
+a = tk.PhotoImage(file="assets/apple.gif")
+seed()
+canvas.create_image(100, 100, image=a, anchor=tk.NW)
 
 root.mainloop()
