4ae53b6fd85bce713a942436383407d68d8718a8
Kshitij <kshitijchaudhari@gmail.com>
2021-04-28 08:38:46 +0530

    Seperated for python programs (generic) only

--- excelR/ex2.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('c:/development/excelr/datasets/mtcars.csv')
-filter1 = df['cyl']>4
-filter2 = df['hp']>100
-df.where(filter1 & filter2, inplace=True)
-print(df)
-
--- excelR/ex3.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('c:/development/excelr/datasets/salaries.csv')
-print(df.describe())
-print(df.phd.describe())
-print(df.phd.count())
-print(df.phd.mean())
-
